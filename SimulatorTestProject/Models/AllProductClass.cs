91208bdecce6f77ff3c8838368cdcf69604e9277
M. Ghannan <msghannan@hotmail.com>
2020-10-12 13:09:17 +0200

    Add project files.

---
+++ SimulatorTestProject/Models/AllProductClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimulatorTestProject.Models
+{
+    public class AllProductClass
+    {
+        public List<PipeClass> PipeList { get; set; }
+        public List<PumpClass> PumpList { get; set; }
+        public List<VentilClass> VentilList { get; set; }
+    }
+}
---
+++ SimulatorTestProject/Models/PipeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimulatorTestProject.Models
+{
+    public class PipeClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Status { get; set; }
+        //public double PipeLenght { get; set; } // imp. senare
+        //public double PipeDiameter { get; set; } //imp. senare
+    }
+}
